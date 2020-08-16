13f43cc6c8ad8c40660a6231f8c002620188deba
shreya <shreya2105@gmail.com>
2020-08-16 16:03:01 +0100

    updates files

--- ProductReviews/amazon_user_reviews/spiders/amazon_reviews.py
+++ ProductReviews/amazon_user_reviews/spiders/amazon_reviews.py
@@ -1,5 +1,6 @@
 #source:https://blog.datahut.co/scraping-amazon-reviews-python-scrapy/
 import scrapy
+
 # Creating a new class to implement Spide
 class AmazonReviewsSpider(scrapy.Spider):
  
@@ -10,11 +11,11 @@ class AmazonReviewsSpider(scrapy.Spider):
     allowed_domains = ['amazon.in']
  
     # Base URL for the MacBook air reviews
-    myBaseUrl = "https://www.amazon.in/Maggi-2-Minute-Noodles-Masala-Pack/product-reviews/B07B4KQRZG/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&amp;amp;pageNumber="
+    myBaseUrl = "https://www.amazon.in/JBL-T205BT-Wireless-Earbud-Headphones/product-reviews/B07B9G75Z9/ref=cm_cr_arp_d_paging_btm_next_2?pageNumber="
     start_urls=[]
  
     # Creating list of urls to be scraped by appending page number a the end of base url
-    for i in range(1,176):
+    for i in range(1,500):
         start_urls.append(myBaseUrl+str(i))
     #print (start_urls)
  
@@ -32,16 +33,11 @@ class AmazonReviewsSpider(scrapy.Spider):
             review_date = data.css('.review-date')
             
             count = 0
- 
-            # Combining the results
-            #for review in star_rating:
-            #    yield{'stars': ''.join(review.xpath('.//text()').extract()),
-                      
-            #          'comment': ''.join(comments[count].xpath(".//text()").extract())}
-                
+      
             for review in star_rating:
                 yield{'stars': ''.join(review.xpath('.//text()').extract()),
-                      'comment': ''.join(comments[count].xpath(".//text()").extract())
+                      'comment': ''.join(comments[count].xpath(".//text()").extract()),
+                      'date': ''.join(review_date[count].xpath(".//text()").extract())
                      }
                 count=count+1
                 
